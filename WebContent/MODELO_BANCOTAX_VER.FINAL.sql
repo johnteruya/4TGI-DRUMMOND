3b6b9ffb2f7741e09d13374bc68815d8729c45f4
johnteruya <jteruya9@gmail.com>
2018-10-23 17:00:50 -0300

    inserindo projeto

--- WebContent/MODELO_BANCOTAX_VER.FINAL.sql
+++ WebContent/MODELO_BANCOTAX_VER.FINAL.sql
@@ -1,9 +1,3 @@
-﻿# 4º TGI-DRUMMOND
-Trabalho de Integração de Grupo - Simulação de um Sistema Bancário.
-
-## Banco de Dados - Oracle SQL
-### Banco referente a Taxas:
-
 CREATE TABLE taxas(
 codTaxa NUMBER(3) PRIMARY KEY,
 tipoTaxa VARCHAR2(30) NOT NULL,
@@ -11,14 +5,14 @@ valorTaxa NUMBER(3,2) NOT NULL,
 dataVigencia DATE
 );
 
-#### Comandos de Interações Banco:
+
+ALTER TABLE taxas MODIFY dataVigencia VARCHAR2(10) NOT NULL;
+
+ALTER TABLE taxas MODIFY dataVigencia DATE;
 
 INSERT INTO taxas VALUES ( 500, 'EMPRESTIMO - JUROS CONTRATO', 3.5, '21-11-2017');
 INSERT INTO taxas VALUES ( 600, 'EMPRESTIMO - JUROS MORA', 2.3, '21-11-2017');
 INSERT INTO taxas VALUES ( 700, 'EMPRESTIMO - JUROS ATRASO', 1.5, '21-11-2017');
-
-ALTER TABLE taxas MODIFY dataVigencia VARCHAR2(10) NOT NULL;
-ALTER TABLE taxas MODIFY dataVigencia DATE;
 
 DELETE FROM Taxas
 
